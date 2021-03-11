ce22539b1ad718554875346d07d77cdae2dedf5f
Fernando García <fernando.garciagomez.01@telefonica.com>
2021-03-11 21:36:24 +0100

    Catálogo de cochones Paco

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    function order($a, $b) {
+        return $a->name > $b->name;
+    }
+    function getMedia($dir) {
+        $files = [];
+        if ($gestor = opendir($dir)) {
+            while (false !== ($entry = readdir($gestor))) {
+                $extension = strtolower( substr($entry, -4) );
+                if ($extension === '.jpg') {
+                    $file = new stdclass();
+                    $file->name = $entry;
+                    $file->extension = $extension;
+                    $files[] = $file;
+                }
+            }
+        }
+        usort($files, order);
+        return $files;
+    }
+?><!DOCTYPE html>
+<html class="no-js" lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunidad Colchonera</title>
+    <style type="text/css">
+        body{ margin: 0; padding: 0; font-family: Arial, Helvetica, sans-serif; font-size: 75%; }
+        #lienzo { column-count: 6; column-gap: 1px; }
+        figure { margin: 0 0 1px 0; width: 100%; text-align: center; position: relative; }
+        img, video{ display: block; width: 100%; height: auto; z-index: 0; }
+        figcaption{ position: absolute; z-index: 10; bottom: 0; left: 0; right: 0; text-align: center; padding: .2em; background-color: rgba(0,0,0,.5); color: white; font-size: 80%; }
+
+        @media screen and (max-width: 800px) {
+                #lienzo { column-count: 4; }
+        }
+        @media screen and (max-width: 500px) {
+                #lienzo { column-count: 2; }
+        }
+    </style>
+</head>
+<body>
+    <div id="lienzo">
+<?php
+        $medias = getMedia(__DIR__);
+        foreach($medias as $media) {
+            switch($media->extension) {
+                case '.jpg':
+?>
+                    <figure><a href="./<?=$media->name?>"><img src="./<?=$media->name?>"><figcaption><?=$media->name?></figcaption></a></figure>
+<?php
+                    break;
+                case '.mp4':
+?>
+                    <figure>Vídeo <?=$media->name?></figure>
+<?php
+                    break;
+            }
+        }
+?>
+    </div>
+</body>
+</html>
